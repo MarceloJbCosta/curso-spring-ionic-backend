4eeb8a1ba1a3e677d8575fd0246ee9d84ce6e18e
marcelojbcosta <marcelomogueco@gmail.com>
2021-09-17 21:10:43 -0300

    Paginacao com parametros opcionais

--- src/main/java/com/marcelocosta/cursomc/CursomcApplication.java
+++ src/main/java/com/marcelocosta/cursomc/CursomcApplication.java
@@ -63,6 +63,13 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Cama messa e banho ");
+		Categoria cat4 = new Categoria(null, "Eletronicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoraçao");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
+		
+		
 		
 		Produto p1 =  new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00 );
@@ -79,7 +86,7 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		
 		//adicionamdo os iten da ctaegoria ao bd
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		//add os itens de produto ao bd
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
