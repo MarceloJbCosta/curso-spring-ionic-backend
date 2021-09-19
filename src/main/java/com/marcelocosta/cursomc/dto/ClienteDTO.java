175cf709c14550c581c43656e187f5529bf4bc3b
marcelojbcosta <marcelomogueco@gmail.com>
2021-09-19 09:42:18 -0300

    validacao customizada, email nao repedido na atualizacao do cliente

--- src/main/java/com/marcelocosta/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/marcelocosta/cursomc/dto/ClienteDTO.java
@@ -9,7 +9,8 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.marcelocosta.cursomc.domain.Cliente;
-
+import com.marcelocosta.cursomc.services.validation.ClienteUpdate;
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
