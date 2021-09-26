f53707339df0e9a2fdd1cfa342e336e837119af2
marcelojbcosta <marcelomogueco@gmail.com>
2021-09-26 15:09:31 -0300

    configuracao e envio do projeto para producao

--- src/main/java/com/marcelocosta/cursomc/config/DevConfig.java
+++ src/main/java/com/marcelocosta/cursomc/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.marcelocosta.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -16,10 +17,16 @@ public class DevConfig {
 	@Autowired
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
 		
-		
+		if(!"create".equals(strategy)) {
+			return false;
+			
+		}
 		dbService.instantiateDatabase();
 		
 		return true;
