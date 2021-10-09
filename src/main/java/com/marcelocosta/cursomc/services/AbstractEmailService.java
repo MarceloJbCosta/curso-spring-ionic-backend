8b3128368a332403e5942a6da6063ee9786184f4
marcelojbcosta <marcelomogueco@gmail.com>
2021-10-08 21:22:50 -0300

    Criacao htmlEmailService

--- src/main/java/com/marcelocosta/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/marcelocosta/cursomc/services/AbstractEmailService.java
@@ -2,38 +2,75 @@ package com.marcelocosta.cursomc.services;
 
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.marcelocosta.cursomc.domain.Pedido;
 
-public abstract class AbstractEmailService implements EmailService{
-	
+public abstract class AbstractEmailService implements EmailService {
+
 	@Value("${default.sender}")
 	private String sender;
+
+	@Autowired
+	private TemplateEngine templateEngine;
 	
+	@Autowired
+	private JavaMailSender javaMailSender;
+
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
-		
 		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
 		sendEmail(sm);
-		
+
 	}
 
 	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
 		SimpleMailMessage sm = new SimpleMailMessage();
-		
-		sm.setTo(obj.getCliente().getEmail()); //para quem que vai ser enviado o email
-		
-		sm.setFrom(sender);//quem vai enviar  email
-		
-		sm.setSubject("Pedido confirmado! Código: " + obj.getId() );//assunto do email
-		
-		sm.setSentDate(new Date(System.currentTimeMillis()));//data do email
-		
-		sm.setText(obj.toString());//corpo do email
-		
+		sm.setTo(obj.getCliente().getEmail()); // para quem que vai ser enviado o email
+		sm.setFrom(sender);// quem vai enviar email
+		sm.setSubject("Pedido confirmado! Código: " + obj.getId());// assunto do email
+		sm.setSentDate(new Date(System.currentTimeMillis()));// data do email
+		sm.setText(obj.toString());// corpo do email
 		return sm;
 	}
-	
+
+	protected String htmlFromTemplatePedido(Pedido obj) {
+		Context context = new Context(); // pega template
+		context.setVariable("pedido", obj);// injeta o objeto, processa o template
+		return templateEngine.process("email/confirmacaoPedido", context);// retorna o html que tem dentro
+	}
+
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
+		try {
+		MimeMessage mm = prepareMimeMessageFromPedido(obj);
+		sendHtmlEmail(mm);
+		}
+		catch (MessagingException e) {
+			sendOrderConfirmationEmail(obj);
+		}
+	}
+
+	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+		MimeMessage mimeMessage =javaMailSender.createMimeMessage();
+		MimeMessageHelper mmh = new  MimeMessageHelper(mimeMessage, true);
+		
+		mmh.setTo(obj.getCliente().getEmail());
+		mmh.setFrom(sender);
+		mmh.setSubject("Pedido confirmado! Código: " + obj.getId());
+		mmh.setSentDate(new Date(System.currentTimeMillis()));
+		mmh.setText(htmlFromTemplatePedido(obj), true);
+		
+		
+		return mimeMessage;
+	}
 }
--- src/main/java/com/marcelocosta/cursomc/services/EmailService.java
+++ src/main/java/com/marcelocosta/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.marcelocosta.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.marcelocosta.cursomc.domain.Pedido;
@@ -11,6 +13,10 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);//email de confirmacao de emial recebendo um pedido
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 		
 
 }
