5777b5725f2fff2c238b4e3288dd66ec508ada5f
marcelojbcosta <marcelomogueco@gmail.com>
2021-10-19 22:13:30 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/marcelocosta/cursomc/CursomcApplication.java
+++ src/main/java/com/marcelocosta/cursomc/CursomcApplication.java
@@ -1,14 +1,18 @@
 package com.marcelocosta.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.marcelocosta.cursomc.services.S3Service;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
 
-	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -17,7 +21,10 @@ public class CursomcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
+		s3Service.uploadFile("/Users/macbook/Desktop/Curso Spring/fotos/ana.jpg");
 
 	}
+	
+
 
 }
