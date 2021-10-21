9341dc7822b38a34c4e91d5b9b86b186d3b735c3
marcelojbcosta <marcelomogueco@gmail.com>
2021-10-20 21:01:54 -0300

    Enviando imagem via EndPoint

--- src/main/java/com/marcelocosta/cursomc/CursomcApplication.java
+++ src/main/java/com/marcelocosta/cursomc/CursomcApplication.java
@@ -1,18 +1,15 @@
 package com.marcelocosta.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.marcelocosta.cursomc.services.S3Service;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
 
-	@Autowired
-	private S3Service s3Service;
+	//@Autowired
+	//private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -21,7 +18,7 @@ public class CursomcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		s3Service.uploadFile("/Users/macbook/Desktop/Curso Spring/fotos/ana.jpg");
+		//s3Service.uploadFile("/Users/macbook/Desktop/Curso Spring/fotos/ana.jpg");
 
 	}
 	
--- src/main/java/com/marcelocosta/cursomc/services/ClienteService.java
+++ src/main/java/com/marcelocosta/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.marcelocosta.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.marcelocosta.cursomc.domain.Cidade;
 import com.marcelocosta.cursomc.domain.Cliente;
@@ -37,6 +39,9 @@ public class ClienteService {
 	
 	@Autowired
 	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	
 	//operacao para buscar a cliente por id
@@ -115,6 +120,10 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());	
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 
 
